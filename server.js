c5fa57cc28eaba3bd65761d9a80c766379e45669
zhangct456 <zhangct456@163.com>
2018-12-06 10:12:46 +0800

    补注释

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const formidable = require('formidable');
 const fs = require('fs');
 
 router.get('/', (req, res) => {
+	//测试连通性
 	res.send("node server");
 })
 
@@ -16,6 +17,7 @@ router.post('/upload', (req, res) => {
 	var form = new formidable.IncomingForm();
     // 设置文件保存路径，相对路径
     form.uploadDir = "./uploads";
+    
 	form.parse(req, function(err, fields, files) {
 		if(err){
 			res.send({
@@ -24,9 +26,10 @@ router.post('/upload', (req, res) => {
 			});
 			return;
 		}
-        var fileName = files.file.name;
+        var fileName = files.file.name;		//上传文件名
         var fileNameArr = fileName.split('.');
         if(fileNameArr != 1){
+        	//files.file.path：文件保存后的路径，无后缀名
         	//补上文件后缀
         	var newfile = files.file.path + '.' + fileNameArr[fileNameArr.length - 1];
         	fs.rename(files.file.path, newfile, function(err){
